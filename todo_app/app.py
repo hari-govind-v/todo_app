a12bc7ca2bfa26a3bf86ce614cc96630b3462463
hari-govind-v <harigovind.v@keyvalue.systems>
2025-07-23 12:57:37 +0530

    chore(migrations): add schema migrations

--- todo_app/app.py
+++ todo_app/app.py
@@ -8,12 +8,7 @@ from .users.routes import user_router
 from .auth.routes import auth_router
 from .routes import router
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(task_router, prefix="/users/{userid}/tasks")
 app.include_router(user_router, prefix="/users")
